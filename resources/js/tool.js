072b9854c9afb1467a58d6b6c0346223e31cef30
sandermj <59999710+sandermj@users.noreply.github.com>
2021-01-08 21:30:12 +0100

    Update tool.js

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,5 +1,3 @@
 Nova.booting((Vue, router, store) => {
     Vue.component('action-selector', require('./components/ActionSelector'));
-    Vue.component('filter-menu', require('./components/EmptyComponent'));
-    Vue.component('delete-menu', require('./components/EmptyComponent'));
 })
